12189cb126ccff5c52c591e74adacab46079951f
Rkeerthiga <keerthigaputtur21@gmail.com>
2020-05-02 12:20:24 +0530

    Recycler View

---
+++ app/src/main/java/com/example/recyclerview/Layout_list.java
@@ -0,0 +1,14 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_list);
+    }
+}
